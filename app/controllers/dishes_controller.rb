776f67e325ac113d54e227fb8e48623f572e6d81
Phan Quang <phanhaiquang@gmail.com>
2016-09-28 05:05:38 +0000

    add cost to dishes

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -69,6 +69,6 @@ class DishesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def dish_params
-      params.require(:dish).permit(:name, :enable)
+      params.require(:dish).permit(:name, :enable, :cost)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160928041136) do
+ActiveRecord::Schema.define(version: 20160928045220) do
 
   create_table "dishes", force: :cascade do |t|
     t.string   "name"
     t.boolean  "enable"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "cost"
   end
 
   create_table "orders", force: :cascade do |t|
